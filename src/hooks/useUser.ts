c284e419fc8edf7fe49f06579ed4e3563a0c398d
Nubookd <trewqtrewqtsop@gmail.com>
2026-02-21 11:03:33 +0300

    add profile client components

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -31,6 +31,94 @@ export function useUser() {
   const { checkAuth } = useAuth();
 
 
+  const getOrders = useCallback(
+    async (status: OrderStatus, page?: number): Promise<IClientResponse> => {
+      try {
+        setLoading((prev) => ({ ...prev, orders: true }));
+        setError((prev) => ({ ...prev, orders: "" }));
+        const isAuth = await checkAuth();
+        if (!isAuth.success || !isAuth.user) {
+          return {
+            success: false,
+            message: isAuth.message,
+            status: 401,
+          };
+        }
+        const validRole = UserRoleSchema.safeParse(isAuth.user.role);
+        if (!validRole.success) {
+          return formatZodError(validRole.error);
+        }
+        const validStatus = OrderStatusSchema.safeParse(status);
+        if (!validStatus.success) {
+          return formatZodError(validStatus.error);
+        }
+
+        const validPagination = PaginationSchema.safeParse({
+          status,
+          page,
+        });
+        if (!validPagination.success) {
+          return formatZodError(validPagination.error);
+        }
+        const params = new URLSearchParams();
+        params.set("role", validRole.data);
+        if (validPagination.data.status) {
+          params.set("status", validPagination.data.status);
+        }
+        if (validPagination.data.page) {
+          params.set("page", validPagination.data.page.toString());
+        }
+        const res = await fetch(`/api/dashboard/orders?${params.toString()}`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        if (!res.ok) {
+          let errorMessage = `Ошибка HTTP: ${res.status}`;
+
+          const errorData = await res.json();
+          errorMessage = errorData.message || errorMessage;
+
+          setError((prev) => ({ ...prev, orders: errorMessage }));
+          return {
+            success: false,
+            message: errorMessage,
+            status: res.status,
+          };
+        }
+        const data = await res.json();
+        if (data.success && data.orders) {
+          setOrders(data.orders);
+          return {
+            success: data.success,
+            message: data.message,
+            status: data.status,
+            orders: data.orders,
+            meta: data.meta,
+          };
+        } else {
+          setError((prev) => ({
+            ...prev,
+            orders: data.message || "Ошибка загрузки",
+          }));
+          return {
+            success: false,
+            message: data.message || "Ошибка загрузки заказов",
+            status: data.status || 400,
+          };
+        }
+      } catch (error) {
+        return {
+          success: false,
+          message: "Внутренняя ошибка сервера | hook",
+          status: 500,
+        };
+      } finally {
+        setLoading((prev) => ({ ...prev, orders: false }));
+      }
+    },
+    [checkAuth],
+  );
+
   const createOrder = useCallback(
     async (order: ICheque): Promise<IClientResponse> => {
       try {
@@ -67,6 +155,47 @@ export function useUser() {
         };
       } finally {
         setLoading((prev) => ({ ...prev, create: false }));
+      }
+    },
+    [checkAuth],
+  );
+
+  const getDetailsOrder = useCallback(
+    async (orderId: number): Promise<IClientResponse> => {
+      try {
+        setLoading((prev) => ({ ...prev, details: true }));
+        const isValidData = IdSchema.safeParse(orderId);
+        if (!isValidData.success) {
+          const formattedError = formatZodError(isValidData.error);
+          return formattedError;
+        }
+        const isAuth = await checkAuth();
+        if (!isAuth.success) {
+          return {
+            success: false,
+            message: isAuth.message,
+            status: 401,
+          };
+        }
+        const res = await fetch(`/api/dashboard/orders/${orderId}`, {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        return {
+          success: data.success,
+          message: data.message,
+          status: data.status,
+          order: data.order,
+        };
+      } catch (error) {
+        return {
+          success: false,
+          message: "Внутренняя ошибка сервера | hook",
+          status: 500,
+        };
+      } finally {
+        setLoading((prev) => ({ ...prev, details: false }));
       }
     },
     [checkAuth],
